e4898a82760a4c1c98e6c90b8022c0bf09090819
Fabian Colombo <facolomb@c2r5s7.42lausanne.ch>
2022-02-22 14:32:18 +0100

    rotate.c

--- sort_files/rotate.c
+++ sort_files/rotate.c
@@ -14,12 +14,32 @@
 
 void	ft_ra(t_stack *sort)
 {
+	int	new[sort->cola_size];
+	int	i;
 
+	i = sort->cola_size - 1;
+	new[i] = sort->cola[0];
+	while (i > 0)
+	{
+		new[i - 1] = sort->cola[i];
+		i--;
+	}
+	sort->cola = new;
 }
 
 void	ft_rb(t_stack *sort)
 {
+	int	new[sort->colb_size];
+	int	i;
 
+	i = sort->colb_size - 1;
+	new[i] = sort->colb[0];
+	while (i > 0)
+	{
+		new[i - 1] = sort->colb[i];
+		i--;
+	}
+	sort->colb = new;
 }
 
 void	ft_rr(t_stack *sort)
