c17fa20694f097dc7d31c88b87a307633fe87367
fabian <facolomb@student.42lausanne.ch>
2022-02-23 11:58:37 +0100

    sort

--- sort_files/push.c
+++ sort_files/push.c
@@ -14,13 +14,46 @@
 
 void	ft_pa(t_stack *sort)
 {
+	int	newa[sort->cola_size + 1];
+	int	newb[sort->colb_size - 1];
+	int	i;
+
 	if (sort->colb_size > 0)
 	{
-		
+		i = 1;
+		newa[0] = sort->colb[0];
+		while (i < sort->cola_size + 1)
+		{
+			newa[i] = sort->cola[i - 1];
+			newb[i - 1] = sort->colb[i];
+			i++;
+		}
+		sort->cola = newa;
+		sort->colb = newb;
+		sort->cola_size++;
+		sort->colb_size--;
 	}
 }
 
 void	ft_pb(t_stack *sort)
 {
+	int	newa[sort->cola_size - 1];
+	int	newb[sort->colb_size + 1];
+	int	i;
 
+	if (sort->cola_size > 0)
+	{
+		i = 1;
+		newb[0] = sort->cola[0];
+		while (i < sort->colb_size + 1)
+		{
+			newb[i] = sort->colb[i - 1];
+			newa[i - 1] = sort->cola[i];
+			i++;
+		}
+		sort->colb = newb;
+		sort->cola = newa;
+		sort->colb_size++;
+		sort->cola_size--;
+	}
 }
--- sort_files/reverse_rotate.c
+++ sort_files/reverse_rotate.c
@@ -14,12 +14,30 @@
 
 void	ft_rra(t_stack *sort)
 {
+	int	new[sort->cola_size];
+	int	i;
 
+	i = 0;
+	new[0] = sort->cola[sort->cola_size - 1];
+	while (i < sort->cola_size)
+	{
+		new[i + 1] = sort->cola[i];
+	}
+	sort->cola = new;
 }
 
 void	ft_rrb(t_stack *sort)
 {
+	int	new[sort->colb_size];
+	int	i;
 
+	i = 0;
+	new[0] = sort->colb[sort->colb_size - 1];
+	while (i < sort->colb_size)
+	{
+		new[i + 1] = sort->colb[i];
+	}
+	sort->colb = new;
 }
 
 void	ft_rrr(t_stack *sort)
